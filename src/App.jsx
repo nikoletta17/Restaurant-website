2d83f8653b94d6e89ffb71770de3619d903168d8
nikoletta17 <ivanova.nicole.home@gmail.com>
2026-04-09 15:15:50 +0300

    Refactor: move project to root and setup gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import { MenuBanner } from "./components/Banners/MenuBanner";
 import { Testimonial } from "./components/Testimonial/Testimonial";
 import { Hero } from "./components/Hero/Hero";
 import { NavBar2 } from "./components/NavBar/NevBar2";
-/* import { NavBar } from "./components/NavBar/NavBar"; */
 
 
 function App() {
   return (
     <>
       <main>
-        {/*    <NavBar /> */}
         <NavBar2 />
         <Hero />
         <MenuBanner />
