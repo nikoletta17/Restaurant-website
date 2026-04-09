b1f8446b616f471d260b53ff43eeb4da0dd9f2f2
nikoletta17 <ivanova.nicole.home@gmail.com>
2026-04-09 17:54:45 +0300

    Styled about section without animations

--- src/components/Banners/AboutBanner.jsx
+++ src/components/Banners/AboutBanner.jsx
@@ -1,25 +1,32 @@
 import React from "react";
-import MenuImg1 from "../../assets/banner/Menu1.png";
+import AboutImg from "../../assets/AboutImg.png";
 
 export const AboutBanner = () => {
   return (
     <>
-      <section className="overflow-hidden">
+      <section id="about" className="overflow-hidden bg-lightBlue scroll-mt-15">
         <div className="min-h-screen flex justify-center items-center">
           <div className="container mx-auto flex flex-col sm:flex-row items-center justify-around gap-10">
             {/* Img section */}
-            <div className="w-full sm:w-1/2 flex justify-center">
-              <img src={MenuImg1} alt="" />
+            <div className="relative w-full sm:w-1/2 flex justify-center items-center">
+              {/* Semi circle background */}
+              <div className="absolute left-1/2 -translate-x-[100%]  h-[260px] w-[130px] md:h-[350px] md:w-[180px] lg:h-[450px] lg:w-[230px] bg-[#3C3F8F]/90 rounded-l-full z-0" />
+              {/* Img itself */}
+              <img
+                src={AboutImg}
+                alt="About Img"
+                className="relative w-full max-w-[250px] md:max-w-[450px] object-cover rounded-full z-10 shadow-[50px_0_50px_-10px_rgba(0,0,0,0.08)]"
+              />
             </div>
             {/* Content section */}
             <div className="space-y-6 w-full sm:w-1/2 text-center sm:text-left">
-              <span className="inline-block tracking-wide font-bold font-playwrite text-2xl lg:text-3xl text-red-600">
+              <span className="inline-block tracking-wide font-bold font-playwrite text-3xl lg:text-4xl text-[#3C3F8F]">
                 About us
               </span>
-              <h2 className="font-headers text-6xl lg:text-8xl lg:leading-tight">
+              <h2 className="font-headers text-7xl lg:text-8xl lg:leading-tight text-primary">
                 The Facet
               </h2>
-              <p className="text-lg text-gray-600 max-w-lg mx-auto sm:mx-0">
+              <p className="text-xl text-gray-600 max-w-lg mx-auto sm:mx-0">
                 Welcome to The Facet, where culinary excellence meets
                 exceptional service. We are passionate about creating memorable
                 dining experiences that celebrate the rich flavors and
