82b19189684e29467af7b6018c7b1bf95202927a
nikoletta17 <ivanova.nicole.home@gmail.com>
2026-04-28 18:41:03 +0300

    Fix: NavBar cursor follower

--- src/components/NavBar/NevBar.jsx
+++ src/components/NavBar/NevBar.jsx
@@ -36,7 +36,7 @@ export const NavBar = () => {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 w-full z-50 bg-lightBlue">
+      <nav className="fixed top-0 left-0 w-full z-50 bg-transparent">
         <motion.div
           initial={{ opacity: 0, y: -20 }}
           animate={{
@@ -45,7 +45,7 @@ export const NavBar = () => {
             borderBottomColor: isScrolling ? "#4548a09e" : "#697ef3",
           }}
           transition={{ duration: 0.4 }}
-          className="bg-[#696DC6] flex justify-between items-center py-4 px-4 md:px-8 lg:px-16 border-b-3"
+          className="w-full bg-[#696DC6] flex justify-between items-center py-4 px-4 md:px-8 lg:px-16 border-b-3"
         >
           {/* Logo Section */}
           <div className="font-bold text-2xl lg:text-3xl flex gap-2 items-center font-playwrite cursor-default select-none text-white flex-shrink-0">
@@ -62,7 +62,7 @@ export const NavBar = () => {
               onMouseLeave={() =>
                 setPosition((pv) => ({ ...pv, opacity: 0, activeId: null }))
               }
-              className="relative flex items-center w-fit px-2 lg:px-6 py-1 rounded-md border-2 border-black bg-white/80 select-none"
+              className="relative flex items-center w-fit py-1 rounded-md border-2 border-black bg-white/80 select-none"
             >
               {NavbarMenu.map((menu) => (
                 <Tab
@@ -80,7 +80,7 @@ export const NavBar = () => {
                 onMouseEnter={() =>
                   setPosition((pv) => ({ ...pv, opacity: 0, activeId: null }))
                 }
-                className="relative z-10 ml-1 lg:ml-2 text-xl hover:scale-125 duration-300 p-2 text-[#3C3F8F]"
+                className="relative z-10 mx-2 text-xl hover:scale-125 duration-300 p-2 text-[#3C3F8F]"
               >
                 <FaCartShopping />
               </button>
@@ -98,6 +98,7 @@ export const NavBar = () => {
           </div>
         </motion.div>
       </nav>
+
       <div className="md:hidden">
         <ResponsiveMenu open={isOpen} setIsOpen={setIsOpen} />
       </div>
@@ -114,16 +115,19 @@ const Tab = ({ children, setPosition, id, activeId, link }) => {
       ref={ref}
       onMouseEnter={() => {
         if (!ref?.current) return;
-        const { width } = ref.current.getBoundingClientRect();
+        
+        const { width, left: childLeft } = ref.current.getBoundingClientRect();
+        const { left: parentLeft } = ref.current.parentElement.getBoundingClientRect();
+
         setPosition({
-          left: ref.current.offsetLeft,
+          left: childLeft - parentLeft,
           width,
           opacity: 1,
           activeId: id,
         });
       }}
-      className={`relative z-10 block cursor-pointer px-3 py-2 font-bold uppercase transition-colors duration-300 
-      md:px-2 lg:px-5 md:text-sm lg:text-base ${
+      className={`relative z-10 block cursor-pointer px-4 py-2 font-bold uppercase transition-colors duration-300 
+      md:px-3 lg:px-5 md:text-sm lg:text-base ${
         isActive ? "text-white" : "text-[#11125d]"
       }`}
     >
